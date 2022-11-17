ae6c3a942f9d0566d0771b78cf38545f5d71e58c
Alexandre Duarte $ git config --global user.email alexandreduara@gmail.comgit config --global user.name Alexandre <alexandreduara@gmail.com>
2022-11-17 12:49:47 -0300

    ícones menu e escalabilidade

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-const buttonMenu = document.querySelector('.menu__button')
-const menu = document.querySelector('.menu__open')
-const buttonMenu_resources = document.querySelector('.menu__resources--button')
-const buttonMenu_company = document.querySelector('.menu__company--button')
-const menu_resources = document.querySelector('.menu__resources')
-const arrow = document.queryCommandValue('.arrow')
-const menu_company = document.querySelector('.menu__company')
+const buttonMenu = document.querySelector('.menu__button');
+const iconMenu = document.querySelector('.menu__button-img');
+const menu = document.querySelector('.menu__open');
+const buttonMenu_features = document.querySelector('.menu__features--button');
+const buttonMenu_company = document.querySelector('.menu__company--button');
+const menu_features = document.querySelector('.menu__features');
+const menu_company = document.querySelector('.menu__company');
+var arrow__features = document.querySelector('.arrow__features');
+var arrow__company = document.querySelector('.arrow__company');
 
 buttonMenu.addEventListener('click', () => {
     menu.classList.toggle('menu__open--active')
 })
-buttonMenu_resources.addEventListener('click', () => {
-    menu_resources.classList.toggle('menu__resources--off')
+
+buttonMenu_features.addEventListener('click', () => {
+    menu_features.classList.toggle('menu__features--off')
 })
+
 buttonMenu_company.addEventListener('click', () => {
     menu_company.classList.toggle('menu__company--off')
 })
+
+buttonMenu.addEventListener('click',()=>{
+    if (iconMenu.getAttribute("src") == 'images/icon-close-menu.svg') {
+        iconMenu.setAttribute("src","images/icon-menu.svg");
+    }else{
+        iconMenu.setAttribute("src","images/icon-close-menu.svg"); 
+    }
+})
+
+buttonMenu_features.addEventListener('click',()=>{
+    if (arrow__features.getAttribute("src") == 'images/icon-arrow-up.svg') {
+        arrow__features.setAttribute("src","images/icon-arrow-down.svg");
+    }else{
+        arrow__features.setAttribute("src","images/icon-arrow-up.svg"); 
+    }
+})
+
+buttonMenu_company.addEventListener('click',()=>{
+    if (arrow__company.getAttribute("src") == 'images/icon-arrow-up.svg') {
+        arrow__company.setAttribute("src","images/icon-arrow-down.svg");
+    }else{
+        arrow__company.setAttribute("src","images/icon-arrow-up.svg"); 
+    }
+})
+/*<script>
+        var menu = document.querySelector('nav ul');
+        var menuIcon = document.querySelector('nav .menu-icon');
+        var menuIconImg = document.querySelector('nav .menu-icon img');
+
+        menuIcon.addEventListener('click',()=>{
+            menu.classList.toggle('activo');
+            if (menuIconImg.getAttribute("src") == 'assets/images/menu-open-redm.png') {
+                menuIconImg.setAttribute("src","assets/images/menu-close-redm.png");
+            }else{
+                menuIconImg.setAttribute("src","assets/images/menu-open-redm.png"); 
+            }
+        })
+</script>*/
